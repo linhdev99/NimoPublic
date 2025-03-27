abd2ab72f44156e7657df0e660060e7e167d875f
LinhDev <linh.huynh.dev.99@gmail.com>
2025-03-27 23:50:50 +0700

    bo center

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const email = "support@nimoproject.com"; // Thay email tại đây
+    const email = "adhppl99@gmail.com"; // Thay email tại đây
     const effectiveDate = new Date().toISOString().split("T")[0]; // Định dạng YYYY-MM-DD
 
     const emailElements = document.querySelectorAll("#contact-email");
